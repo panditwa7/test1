c2f06e2b151c240d2ee2ed063d9024879ddeb551
Luis Rubio <luis.rubio@ericsson.com>
2020-03-25 12:48:58 +0100

    em20 updates 21

--- ROLES/app_MGR_Addnode/files/install_MGR_Addnode.sh
+++ ROLES/app_MGR_Addnode/files/install_MGR_Addnode.sh
@@ -1,6 +1,6 @@
 version=`ls -td ./CX* | head -1`
 cd ./$version/
-./Install_App.sh -P MGR -C ADDTOSG << EOF
+sudo ./Install_App.sh -P MGR -C ADDTOSG << EOF
 
 y
 EOF
--- ROLES/app_OLM_Addnode/files/install_OLM_Addnode.sh
+++ ROLES/app_OLM_Addnode/files/install_OLM_Addnode.sh
@@ -1,6 +1,6 @@
 version=`ls -td ./CX* | head -1`
 cd ./$version/
-./Install_App.sh -P OLM -C ADDTOSG << EOF
+sudo ./Install_App.sh -P OLM -C ADDTOSG << EOF
 
 y
 EOF
