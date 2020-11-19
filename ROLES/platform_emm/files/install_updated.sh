bcabdf7b09c347e14ac9fe24d785b5f3cac18c11
Luis Rubio <luis.rubio@ericsson.com>
2020-11-19 16:38:26 +0100

    automate disk names 2

--- ROLES/platform_emm/files/install_updated.sh
+++ ROLES/platform_emm/files/install_updated.sh
@@ -1,11 +1,15 @@
 # Use this file in case of external disks are going to be used
 # If -F VRTS: from vxdisk list output update <mmstorage disk> and <mmdb disk>
 # If IO_Fencing_Type: CPS also update 3 io fencing disks from vxdisk list output
-# If -F NFS or Upgrades or -O NODG, then remove the lines for disks
 vxdisk list
 sudo ./Install_Platform.sh -C $1 -E $2 $3 << EOF
 y
 y
+mmstorage_disk
+mmdb_disk
+io_fencing_disk1
+io_fencing_disk2
+io_fencing_disk3
 y
 y
 y
